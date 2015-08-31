e9d193fe0775bb66d09f8b26d04c2d8f76836c3b
Gabriel <gabriel@coldoc.co>
2015-08-31 16:25:03 -0400

    changed entrypoint to node for node/iojs compatibility this should not affect anything!

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export mongo="mongodb://${MONGO_PORT_27017_TCP_ADDR}:${MONGO_PORT_27017_TCP_PORT}/lissome"
 export redis="redis://${REDIS_PORT_6379_TCP_ADDR}:${REDIS_PORT_6379_TCP_PORT}/0"
-iojs lib/app.js
+node lib/app.js
